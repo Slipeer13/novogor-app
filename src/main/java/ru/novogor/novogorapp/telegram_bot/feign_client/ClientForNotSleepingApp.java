d91963a6ef850ddd4d0d03c724b2a408bc10477c
Slipeer <slipeer1@list.ru>
2023-08-25 17:05:46 +0500

    refact:изменения метода на асинхронный

--- src/main/java/ru/novogor/novogorapp/telegram_bot/feign_client/ClientForNotSleepingApp.java
+++ src/main/java/ru/novogor/novogorapp/telegram_bot/feign_client/ClientForNotSleepingApp.java
@@ -1,15 +1,12 @@
 package ru.novogor.novogorapp.telegram_bot.feign_client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@EnableAsync
+
 @FeignClient(name = "client", url="${url.app}")
 public interface ClientForNotSleepingApp {
 
-    @Async
     @GetMapping("/")
     void checkForNotSleeping();
 }
