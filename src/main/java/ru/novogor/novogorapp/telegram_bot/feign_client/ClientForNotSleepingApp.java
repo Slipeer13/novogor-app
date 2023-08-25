286a1bc028cca3ba3359ffcdf1ddaf1e0cedcf9d
Slipeer <slipeer1@list.ru>
2023-08-25 16:56:56 +0500

    refact:добавление вызова ссылки приложения через определенное время

--- src/main/java/ru/novogor/novogorapp/telegram_bot/feign_client/ClientForNotSleepingApp.java
+++ src/main/java/ru/novogor/novogorapp/telegram_bot/feign_client/ClientForNotSleepingApp.java
@@ -3,15 +3,13 @@ package ru.novogor.novogorapp.telegram_bot.feign_client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @EnableAsync
-@FeignClient(url="${url.app}")
+@FeignClient(name = "client", url="${url.app}")
 public interface ClientForNotSleepingApp {
 
     @Async
-    @Scheduled(fixedRate = 20 * 60000)
     @GetMapping("/")
     void checkForNotSleeping();
 }
